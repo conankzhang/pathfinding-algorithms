c5d66bf7949ff822242e457fa0131e09000e479e
conankzhang <u0409453@utah.edu>
2019-02-28 01:36:29 -0700

    Heuristic: Set up simple heuristics

--- src/Pathfinding/Heuristic.h
+++ src/Pathfinding/Heuristic.h
@@ -1,7 +1,22 @@
 #pragma once
+
+//=======================================================================================================================
 class CHeuristic
 {
 public:
 	virtual float GetEstimate(int InNode) = 0;
 };
 
+//=======================================================================================================================
+class CZeroHeuristic : public CHeuristic
+{
+public:
+	virtual float GetEstimate(int InNode) override;
+};
+
+//=======================================================================================================================
+class CEuclideanHeuristic : public CHeuristic
+{
+public:
+	virtual float GetEstimate(int InNode) override;
+};
