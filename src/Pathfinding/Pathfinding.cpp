dbe5e4570cfd0e532d8b1eeea4fc9b5a46151a49
conankzhang <u0409453@utah.edu>
2019-02-28 00:24:14 -0700

    Pathfinding: Set up algorithm

--- src/Pathfinding/Pathfinding.cpp
+++ src/Pathfinding/Pathfinding.cpp
@@ -10,3 +10,10 @@ CPathfinding::CPathfinding()
 CPathfinding::~CPathfinding()
 {
 }
+
+std::vector<CDirectedWeightedEdge> CPathfinding::FindPath(int StartNode, int GoalNode, CDirectedWeightedGraph* Graph, CHeuristic* Heuristic)
+{
+	std::vector<CDirectedWeightedEdge> Path;
+
+	return Path;
+}
--- src/Pathfinding/Pathfinding.h
+++ src/Pathfinding/Pathfinding.h
@@ -1,8 +1,23 @@
 #pragma once
+
+#include "DirectedWeightedGraph.h"
+
+#include <vector>
+
+
+class CHeuristic
+{
+public:
+	float GetEstimate(int InNode);
+};
+
+
 class CPathfinding
 {
 public:
 	CPathfinding();
 	~CPathfinding();
+
+	std::vector<CDirectedWeightedEdge> FindPath(int StartNode, int GoalNode, CDirectedWeightedGraph* Graph, CHeuristic* Heuristic);
 };
 
